10d260f6aa552f6757f8675c1239b512830a37bb
Ilya Builuk <ilya.builuk@gmail.com>
2015-12-01 22:42:10 +0100

    tests: fix path issues

--- test/config.hpp
+++ test/config.hpp
@@ -1,3 +1,9 @@
 #define TEST_ASSETS_PATH "_TEST_ASSETS_PATH_"
-#define TEST_PBF_FILE TEST_ASSETS_PATH "berlin.osm.pbf"
-#define TEST_XML_FILE TEST_ASSETS_PATH "berlin.osm.xml"
+
+#define TEST_PBF_FILE TEST_ASSETS_PATH "osm/berlin.osm.pbf"
+#define TEST_XML_FILE TEST_ASSETS_PATH "osm/berlin.osm.xml"
+
+#define TEST_SHAPE_POINT_FILE TEST_ASSETS_PATH "shape/point"
+#define TEST_SHAPE_LINE_FILE TEST_ASSETS_PATH "shape/line"
+#define TEST_SHAPE_POLY_FILE TEST_ASSETS_PATH "shape/poly"
+#define TEST_SHAPE_MULTIPOLY_FILE TEST_ASSETS_PATH "shape/multipoly"
