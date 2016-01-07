14667104fc9d807d15bc26273b960e5395a84306
Ilya Builuk <ilya.builuk@gmail.com>
2016-01-08 00:37:52 +0100

    testing: commit progress on ElementStoreTest

--- test/index/ElementStoreTest.cpp
+++ test/index/ElementStoreTest.cpp
@@ -1,3 +1,8 @@
+#include "entities/Element.hpp"
+#include "entities/Node.hpp"
+#include "entities/Way.hpp"
+#include "entities/Area.hpp"
+#include "entities/Relation.hpp"
 #include "index/ElementStore.hpp"
 #include "mapcss/StyleProvider.hpp"
 
@@ -5,14 +10,37 @@
 
 #include "test_utils/MapCssUtils.hpp"
 
+using namespace utymap::entities;
 using namespace utymap::index;
 using namespace utymap::mapcss;
+
+class TestElementStore : public ElementStore
+{
+public:
+    TestElementStore(const StyleProvider& styleProvider, StringTable& stringTable) :
+        ElementStore(styleProvider, stringTable),
+        lastElement(),
+        lastQuadKey()
+    {
+    }
+
+    Element lastElement;
+    utymap::QuadKey lastQuadKey;
+
+protected:
+    void store(const Element& element, const utymap::QuadKey& quadKey)
+    {
+        lastElement = element;
+        lastQuadKey = quadKey;
+    }
+};
 
 struct Index_ElementStoreFixture
 {
     Index_ElementStoreFixture() :
         stringTablePtr(new StringTable("")),
-        styleProviderPtr(nullptr)
+        styleProviderPtr(nullptr),
+        elementStorePtr()
     {
         BOOST_TEST_MESSAGE("setup fixture");
     }
@@ -20,22 +48,30 @@ struct Index_ElementStoreFixture
     ~Index_ElementStoreFixture()
     {
         BOOST_TEST_MESSAGE("teardown fixture");
-        delete stringTablePtr;
+        delete elementStorePtr;
         delete styleProviderPtr;
+        delete stringTablePtr;
         std::remove("string.idx");
         std::remove("string.dat");
     }
 
+    void createElementStore(std::string stylesheet)
+    {
+        styleProviderPtr = MapCssUtils::createStyleProviderFromString(*stringTablePtr, stylesheet);
+        elementStorePtr = new TestElementStore(*styleProviderPtr, *stringTablePtr);
+    }
+
     StringTable* stringTablePtr;
     StyleProvider* styleProviderPtr;
+    TestElementStore* elementStorePtr;
 };
 
 BOOST_FIXTURE_TEST_SUITE(Index_ElementStore, Index_ElementStoreFixture)
 
 BOOST_AUTO_TEST_CASE(GivenBigWay_WhenStore_GeometryIsClipped)
 {
-    styleProviderPtr = MapCssUtils::createStyleProviderFromString(*stringTablePtr, 
-        "way|z1[test=Foo] { key:val; }");
+    Way way;
+    createElementStore("way|z1[test=Foo] { key:val; }");
 
     // TODO
 }
