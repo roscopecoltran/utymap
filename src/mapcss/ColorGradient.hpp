9e433b72eb2d2cf1b73402af3bc99ab1e0db0fb1
Ilya Builuk <ilya.builuk@gmail.com>
2016-01-26 23:41:28 +0100

    gradient: commit progress

--- src/mapcss/ColorGradient.hpp
+++ src/mapcss/ColorGradient.hpp
@@ -22,14 +22,29 @@ public:
     {
     }
 
-    inline float evaluate(float time) const
+    inline std::uint32_t evaluate(float time) const
     {
-        // TODO
-        return 0;
+        GradientData::size_type index = 0;
+        for (; index < colors_.size(); ++index) {
+            if (colors_[index].first > time) {
+                break;
+            }
+        }
+        auto y1 = colors_[index != 0 ? index - 1 : 0].second;
+        auto y2 = colors_[index].second;
+        float mu = time;
+        // TODO find correct mu
+        return interpolate(y1, y2, time);
     }
 
 private:
     GradientData colors_;
+
+    // So far, using linear interpolation algorithm as the fastest.
+    inline std::uint32_t interpolate(std::uint32_t y1, std::uint32_t y2, float mu) const
+    { 
+        return static_cast<std::uint32_t>(y1 * (1 - mu) + y2 * mu); 
+    }
 };
 
 }}
--- test/utils/GradientUtilsTest.cpp
+++ test/utils/GradientUtilsTest.cpp
@@ -10,22 +10,22 @@ using namespace utymap::utils;
 
 BOOST_AUTO_TEST_SUITE(Utils_GradientUtils)
 
-BOOST_AUTO_TEST_CASE(GivenSimpleGradientString_WhenParse_ThenReturnGradient)
+BOOST_AUTO_TEST_CASE(GivenSimpleGradientString_WhenEvaluate_ThenReturnCorrectColor)
 {
      std::string gradientKey = "gradient(#0fffff, #099999 50%, #033333 70%, #000000)";
 
      ColorGradient gradient = GradientUtils::parseGradient(gradientKey);
 
-     BOOST_CHECK_GT(gradient.evaluate(0.5), 0);
+     BOOST_CHECK_EQUAL(gradient.evaluate(0.5), 0x033333);
 }
 
 BOOST_AUTO_TEST_CASE(GivenHexColorString_WhenParse_ThenReturnColor)
 {
-    std::string colorStr = "#aa0000ff";
+    std::string colorStr = "#aa0000";
 
     std::uint32_t color = GradientUtils::parseColor(colorStr);
 
-    BOOST_CHECK_EQUAL(color, 0xAA0000FF);
+    BOOST_CHECK_EQUAL(color, 0xAA0000);
 }
 
 BOOST_AUTO_TEST_CASE(GivenNamedColorString_WhenParse_ThenReturnColor)
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_CASE(GivenNamedColorString_WhenParse_ThenReturnColor)
 
     std::uint32_t color = GradientUtils::parseColor(colorStr);
 
-    BOOST_CHECK_EQUAL(color, 0xFF0000FF);
+    BOOST_CHECK_EQUAL(color, 0xFF0000);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
