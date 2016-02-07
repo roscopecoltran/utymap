ad1e9d0c3d4c33c2b92c7068d200d12ba43ea0ed
Ilya Builuk <ilya.builuk@gmail.com>
2016-02-07 15:44:03 +0100

    tests: refactor test on data to build all tiles

--- test/ExportLibTest.cpp
+++ test/ExportLibTest.cpp
@@ -33,27 +33,31 @@ struct ExportLibFixture {
 
 BOOST_FIXTURE_TEST_SUITE(ExportLib, ExportLibFixture)
 
-BOOST_AUTO_TEST_CASE(GivenTestData_WhenTileLoad_ThenCallbacksAreCalled)
+BOOST_AUTO_TEST_CASE(GivenTestData_WhenAllTilesAreLoaded_ThenCallbacksAreCalled)
 {
-    ::loadTile(0, 0, 1,
-        [](const char* name,
-           const double* vertices, int vertexCount,
-           const int* triangles, int triCount,
-           const int* colors, int colorCount) {
-            BOOST_CHECK_GT(vertexCount, 0);
-            BOOST_CHECK_GT(triCount, 0);
-            BOOST_CHECK_GT(colorCount, 0);
-        },
-        [](uint64_t id, const char* name,
-            const char** tags, int size,
-            const double* vertices, int vertexCount) {
-            // TODO basic checks
-        },
-        [](const char* message)
-        {
-            BOOST_TEST_FAIL(message);
+    for (int i = 0; i <= 1; ++i) {
+        for (int j = 0; j <= 1; j++) {
+            ::loadTile(i, j, 1,
+            [](const char* name,
+               const double* vertices, int vertexCount,
+               const int* triangles, int triCount,
+               const int* colors, int colorCount) {
+                 BOOST_CHECK_GT(vertexCount, 0);
+                 BOOST_CHECK_GT(triCount, 0);
+                 BOOST_CHECK_GT(colorCount, 0);
+            },
+            [](uint64_t id, const char* name,
+               const char** tags, int size,
+               const double* vertices, int vertexCount) {
+               // TODO basic checks
+            },
+            [](const char* message)
+            {
+                 BOOST_TEST_FAIL(message);
+            }
+            );
         }
-    );
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
