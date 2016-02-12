953555dfa4e818a30587be414f342f3c43a74493
Ilya Builuk <ilya.builuk@gmail.com>
2016-02-12 21:43:04 +0100

    utils: add measure function

--- src/utils/CoreUtils.hpp
+++ src/utils/CoreUtils.hpp
@@ -1,6 +1,7 @@
 #ifndef UTILS_COREUTILS_HPP_DEFINED
 #define UTILS_COREUTILS_HPP_DEFINED
 
+#include <chrono>
 #include <string>
 #include <sstream>
 
@@ -12,6 +13,20 @@ inline std::string toString(T t) {
     stream << t;
     return stream.str();
 }
+
+template<typename TimeT = std::chrono::milliseconds>
+struct measure
+{
+    template<typename F, typename ...Args>
+    static typename TimeT::rep execution(F&& func, Args&&... args)
+    {
+        auto start = std::chrono::system_clock::now();
+        std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
+        auto duration = std::chrono::duration_cast< TimeT>
+                            (std::chrono::system_clock::now() - start);
+        return duration.count();
+    }
+};
 
 }}
 
