4626be9c49e3ed504efdb0b6211ce932074e8c9f
Ilya Builuk <ilya.builuk@gmail.com>
2016-02-21 21:40:11 +0100

    formats: add test for osm multipolygon processor

--- test/formats/osm/MultipolygonProcessorTest.cpp
+++ test/formats/osm/MultipolygonProcessorTest.cpp
@@ -179,4 +179,66 @@ BOOST_AUTO_TEST_CASE(GivenOneOuterNonClosedAndTwoInnerClosed_WhenProcess_ThenRet
     BOOST_CHECK(areaMap[4]->coordinates == reinterpret_cast<const Area&>(*relation.elements[1]).coordinates);
 }
 
+BOOST_AUTO_TEST_CASE(GivenMultiplyOuterAndMultiplyInner_WhenProcess_ThenReturnCorrectResult)
+{
+    // see fig.6 http://wiki.openstreetmap.org/wiki/Talk:Relation:multipolygon
+
+    RelationMembers relationMembers = createRelationMembers({
+        std::make_tuple(1, "way", "outer"),
+        std::make_tuple(2, "way", "outer"),
+        std::make_tuple(3, "way", "outer"),
+        std::make_tuple(4, "way", "outer"),
+
+        std::make_tuple(5, "way", "inner"),
+        std::make_tuple(6, "way", "inner"),
+        std::make_tuple(7, "way", "inner"),
+        std::make_tuple(8, "way", "inner"),
+        std::make_tuple(9, "way", "inner"),
+        std::make_tuple(10, "way", "inner"),
+        std::make_tuple(11, "way", "inner"),
+
+        std::make_tuple(12, "way", "outer"),
+        std::make_tuple(13, "way", "outer"),
+        std::make_tuple(14, "way", "outer"),
+        std::make_tuple(15, "way", "outer"),
+
+        std::make_tuple(16, "way", "inner"),
+        std::make_tuple(17, "way", "inner"),
+        std::make_tuple(18, "way", "inner"),
+        std::make_tuple(19, "way", "inner"),
+
+        std::make_tuple(20, "way", "outer")
+    });
+    Tags tags = { utymap::formats::Tag{ "type", "multipolygon" }, utymap::formats::Tag{ "tag", "tags" } };
+    wayMap[1] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 1, 5 }, { 8, 4 }})));
+    wayMap[2] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 8, 4 }, { 9, -1 }})));
+    wayMap[3] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 9, -1 }, { 8, -6 }, { 2, -5 } })));
+    wayMap[4] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 2, -5 }, { 0, -3 }, { 1, 5 } })));
+
+    wayMap[5] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 2, 1 }, { 3, 3 }, { 6, 3 } })));
+    wayMap[6] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 6, 3 }, { 4, 0 }, { 2, 1 } })));
+    wayMap[7] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 1, -2 }, { 3, -1 } })));
+    wayMap[8] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 3, -1 }, { 4, -4 } })));
+    wayMap[9] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 4, -4 }, { 1, -3 } })));
+    wayMap[10] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 1, -3 }, { 1, -2 } })));
+    areaMap[11] = std::shared_ptr<Area>(new Area(ElementUtils::createElement<Area>(*stringTablePtr, {}, { { 6, -3 }, { 7, -1 }, { 8, -4 }, { 6, -3 } })));
+
+    wayMap[12] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 10, 5 }, { 14, 5 } })));
+    wayMap[13] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 14, 5 }, { 14, -1 } })));
+    wayMap[14] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 14, -1 }, { 10, -1 } })));
+    wayMap[15] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 10, -1 }, { 10, 5 } })));
+
+    wayMap[16] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 11, 4 }, { 13, 4 } })));
+    wayMap[17] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 13, 4 }, { 13, 0 }, { 11, 0 } })));
+    wayMap[18] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 11, 0 }, { 12, 2 } })));
+    wayMap[19] = std::shared_ptr<Way>(new Way(ElementUtils::createElement<Way>(*stringTablePtr, {}, { { 12, 2 }, { 11, 4 } })));
+
+    areaMap[20] = std::shared_ptr<Area>(new Area(ElementUtils::createElement<Area>(*stringTablePtr, {}, { { 10, -3 }, { 14, -3 }, { 14, -6 }, { 10, -6 }, { 10, -3 } })));
+    MultipolygonProcessor processor(0, relationMembers, tags, *stringTablePtr, areaMap, wayMap);
+
+    Relation relation = processor.process();
+
+    BOOST_CHECK_EQUAL(7, relation.elements.size());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
