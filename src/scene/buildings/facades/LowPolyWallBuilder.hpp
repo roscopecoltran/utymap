0c1422296280802c57f983fe9486a69031a82006
Ilya Builuk <ilya.builuk@gmail.com>
2016-02-23 22:40:30 +0100

    buildings: use color in low poly wall builder

--- src/scene/buildings/facades/LowPolyWallBuilder.hpp
+++ src/scene/buildings/facades/LowPolyWallBuilder.hpp
@@ -4,6 +4,7 @@
 #include "GeoCoordinate.hpp"
 #include "meshing/MeshTypes.hpp"
 #include "mapcss/ColorGradient.hpp"
+#include "utils/GradientUtils.hpp"
 
 namespace utymap { namespace scene {
 
@@ -23,6 +24,9 @@ public:
     // Sets height above ground level.
     inline LowPolyWallBuilder& setMinHeight(double minHeight) { minHeight_ = minHeight; return *this; }
 
+    // Sets color freq.
+    inline LowPolyWallBuilder& setColorNoise(double freq) { colorNoiseFreq_ = freq; return *this; }
+
     void build(const utymap::GeoCoordinate& start, const utymap::GeoCoordinate& end)
     {
         addPlane(start, end);
@@ -33,7 +37,10 @@ private:
     void addPlane(const utymap::GeoCoordinate& p1, const utymap::GeoCoordinate& p2)
     {
         double top = minHeight_ + height_;
-        int color = 0, index = mesh_.triangles.size();
+        auto index = mesh_.triangles.size();
+        int color = utymap::utils::GradientUtils
+            ::getColor(gradient_, p1.longitude, p1.latitude, height_, colorNoiseFreq_);
+
         // first triangle
         addVertex(p1, minHeight_, index++, color);
         addVertex(p2, minHeight_, index++, color);
@@ -56,7 +63,7 @@ private:
 
     const utymap::meshing::Mesh& mesh_;
     const utymap::mapcss::ColorGradient& gradient_;
-    double height_, minHeight_;
+    double height_, minHeight_, colorNoiseFreq_;
 };
 
 }}
