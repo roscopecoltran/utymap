6bd9a46d1f0e50b78c128224605e0b92487ad087
Ilya Builuk <ilya.builuk@gmail.com>
2016-05-26 23:33:03 +0200

    core: refactoring in building relation processor

--- core/src/formats/osm/BuildingProcessor.hpp
+++ core/src/formats/osm/BuildingProcessor.hpp
@@ -5,6 +5,7 @@
 #include "entities/Element.hpp"
 #include "formats/FormatTypes.hpp"
 #include "formats/osm/OsmDataContext.hpp"
+#include "utils/ElementUtils.hpp"
 
 #include <memory>
 #include <unordered_map>
@@ -19,13 +20,45 @@ public:
                       utymap::formats::RelationMembers& members,
                       const utymap::formats::Tags& tags,
                       utymap::index::StringTable& stringTable,
-                      utymap::formats::OsmDataContext context);
+                      utymap::formats::OsmDataContext context) 
+        : id_(id), members_(members), tags_(tags),
+          stringTable_(stringTable), context_(context)
+    {
+    }
 
-    void process();
+    void BuildingProcessor::process()
+    {
+        std::shared_ptr<utymap::entities::Relation> relation(new utymap::entities::Relation());
+        relation->id = id_;
+        relation->tags = utymap::utils::convertTags(stringTable_, tags_);
+
+        // remove children references
+        utymap::utils::visitRelationMembers(context_, members_, *this);
+
+        context_.relationMap[id_] = relation;
+    }
+
+    void visit(OsmDataContext::NodeMapType::const_iterator node)
+    {
+        context_.nodeMap.erase(node->first);
+    }
+
+    void visit(OsmDataContext::WayMapType::const_iterator way)
+    {
+        context_.wayMap.erase(way->first);
+    }
+
+    void visit(OsmDataContext::AreaMapType::const_iterator area)
+    {
+        context_.areaMap.erase(area->first);
+    }
+
+    void visit(OsmDataContext::RelationMapType::const_iterator rel)
+    {
+        context_.relationMap.erase(rel->first);
+    }
 
 private:
-
-    void removeBuildingParts();
 
     std::uint64_t id_;
     utymap::formats::RelationMembers& members_;
