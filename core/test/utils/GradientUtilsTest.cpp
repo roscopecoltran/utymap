3d6c1462a7aac75f95e831e684942d171bd48f24
Ilya Builuk <ilya.builuk@gmail.com>
2016-06-04 22:28:14 +0200

    core: fix compilation issue

--- core/test/utils/GradientUtilsTest.cpp
+++ core/test/utils/GradientUtilsTest.cpp
@@ -11,9 +11,9 @@ BOOST_AUTO_TEST_SUITE(Utils_GradientUtils)
 BOOST_AUTO_TEST_CASE(GivenSimpleGradientString_WhenEvaluate_ThenReturnCorrectColor)
 {
      std::string gradientKey = "gradient(#0fffff, #099999 50%, #033333 70%, #000000)";
-     ColorGradient gradient = GradientUtils::parseGradient(gradientKey);
+     auto gradient = GradientUtils::parseGradient(gradientKey);
 
-     auto color = gradient.evaluate(0.5);
+     auto color = gradient->evaluate(0.5);
 
      BOOST_CHECK_EQUAL(color, 0x099999FF);
 }
@@ -39,12 +39,11 @@ BOOST_AUTO_TEST_CASE(GivenNamedColorString_WhenParse_ThenReturnColor)
 BOOST_AUTO_TEST_CASE(GivenNonStandardNamedColorStrings_WhenEvaluate_ThenReturnCorrectColor)
 {
     std::string gradientKey = "gradient(activeborder, bisque 25%, darkturquoise 50%, papayawhip)";
-    ColorGradient gradient = GradientUtils::parseGradient(gradientKey);
-
-    BOOST_CHECK_EQUAL(gradient.evaluate(0), 0xB4B4B4FF);
-    BOOST_CHECK_EQUAL(gradient.evaluate(0.25), 0xFFE4C4FF);
-    BOOST_CHECK_EQUAL(gradient.evaluate(0.5), 0x00CED1FF);
-    BOOST_CHECK_EQUAL(gradient.evaluate(1), 0xFFEFD5FF);
+    auto gradient = GradientUtils::parseGradient(gradientKey);
+    BOOST_CHECK_EQUAL(gradient->evaluate(0), 0xB4B4B4FF);
+    BOOST_CHECK_EQUAL(gradient->evaluate(0.25), 0xFFE4C4FF);
+    BOOST_CHECK_EQUAL(gradient->evaluate(0.5), 0x00CED1FF);
+    BOOST_CHECK_EQUAL(gradient->evaluate(1), 0xFFEFD5FF);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
