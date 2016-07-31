e026f1c4690e0ffaea6d5b178451be8822405837
Ilya Builuk <ilya.builuk@gmail.com>
2016-07-31 23:42:49 +0200

    core: fix element store tests

--- core/test/index/ElementStoreTest.cpp
+++ core/test/index/ElementStoreTest.cpp
@@ -223,61 +223,27 @@ BOOST_AUTO_TEST_CASE(GivenAreaBiggerThanTile_WhenStore_GeometryIsTheSameAsForTil
     BOOST_CHECK_EQUAL(elementStore.times, 1);
 }
 
-BOOST_AUTO_TEST_CASE(GivenRelationOfPolygonWithHole_WhenStore_AreaIsReturnedWithClippedGeometry)
+BOOST_AUTO_TEST_CASE(GivenRelationOfPolygonWithHole_WhenStore_RelationIsReturnedWithClippedGeometry)
 {
     Area outer = ElementUtils::createElement<Area>(*dependencyProvider.getStringTable(), 0, {},
     { { 5, 10 }, { 20, 10 }, { 20, -10 }, {5, -10} });
     Area inner = ElementUtils::createElement<Area>(*dependencyProvider.getStringTable(), 0, {},
     { { 10, 5 }, { 15, 5 }, { 15, -5 }, { 10, -5 } });
     Relation relation;
+    relation.id = 0;
     relation.tags = std::vector<Tag>{ ElementUtils::createTag(*dependencyProvider.getStringTable(), "test", "Foo") };
     relation.elements.push_back(std::make_shared<Area>(inner));
     relation.elements.push_back(std::make_shared<Area>(outer));
     TestElementStore elementStore(*dependencyProvider.getStringTable(),
         [&](const Element& element, const QuadKey& quadKey) {
+        auto result = reinterpret_cast<const Relation&>(element);
         if (checkQuadKey(quadKey, 1, 1, 0)) {
-            checkGeometry<Area>(reinterpret_cast<const Area&>(element),
-            { 
-                { 20, 10 }, { 20, 0 }, { 15, 0 }, { 15, 5 }, { 10, 5 }, { 10, 0 }, { 5, 0 }, {5, 10}
-            });
+            checkGeometry<Area>(reinterpret_cast<const Area&>(*result.elements[0]), { { 15, 5 }, { 15, 0 }, { 10, 0 }, { 10, 5 } });
+            checkGeometry<Area>(reinterpret_cast<const Area&>(*result.elements[1]), { { 20, 10 }, { 20, 0 }, { 5, 0 }, { 5, 10 } });
         } else if(checkQuadKey(quadKey, 1, 0, 0)) {
-            checkGeometry<Area>(reinterpret_cast<const Area&>(element),
-            {
-                { 10, 0 }, { 10, -5 }, { 15, -5 }, { 15, 0 }, { 20, 0 }, { 20, -10 }, { 5, -10 }, { 5, 0 }
-            });
+            checkGeometry<Area>(reinterpret_cast<const Area&>(*result.elements[0]), { { 15, 0 }, { 15, -5 }, { 10, -5 }, { 10, 0 } });
+            checkGeometry<Area>(reinterpret_cast<const Area&>(*result.elements[1]), { { 20, 0 }, { 20, -10 }, { 5, -10 }, { 5, 0 } });
         } else {
-            BOOST_FAIL("Unexpected quadKey!");
-        }
-    });
-
-    elementStore.store(relation, LodRange(1, 1),
-        *dependencyProvider.getStyleProvider("relation|z1[test=Foo] { key:val; clip: true;}"));
-
-    BOOST_CHECK_EQUAL(elementStore.times, 2);
-}
-
-BOOST_AUTO_TEST_CASE(GivenRelationOfPolygonWithHole_WhenStore_RelationIsReturnedWithClippedGeometry)
-{
-    Area outer = ElementUtils::createElement<Area>(*dependencyProvider.getStringTable(), 0, {},
-    { { 5, 10 }, { 20, 10 }, { 20, -10 }, { 5, -10 } });
-    Area inner = ElementUtils::createElement<Area>(*dependencyProvider.getStringTable(), 0, {},
-    { { 10, 8 }, { 15, 8 }, { 15, 2 }, { 10, 2 } });
-    Relation relation;
-    relation.tags = std::vector<Tag>{ ElementUtils::createTag(*dependencyProvider.getStringTable(), "test", "Foo") };
-    relation.elements.push_back(std::make_shared<Area>(inner));
-    relation.elements.push_back(std::make_shared<Area>(outer));
-    TestElementStore elementStore(*dependencyProvider.getStringTable(),
-        [&](const Element& element, const QuadKey& quadKey) {
-        if (checkQuadKey(quadKey, 1, 1, 0)) {
-            const Relation& relation = reinterpret_cast<const Relation&>(element);
-            BOOST_CHECK_EQUAL(relation.elements.size(), 2);
-            checkGeometry<Area>(reinterpret_cast<const Area&>(*relation.elements[0]), { { 20, 10 }, { 20, 0 }, { 5, 0 }, { 5, 10 } });
-            checkGeometry<Area>(reinterpret_cast<const Area&>(*relation.elements[1]), { { 10, 2 }, { 15, 2 }, { 15, 8 }, { 10, 8 } });
-        }
-        else if (checkQuadKey(quadKey, 1, 0, 0)) {
-            checkGeometry<Area>(reinterpret_cast<const Area&>(element), { { 20, 0 }, { 20, -10 }, { 5, -10 }, { 5, 0 } });
-        }
-        else {
             BOOST_FAIL("Unexpected quadKey!");
         }
     });
