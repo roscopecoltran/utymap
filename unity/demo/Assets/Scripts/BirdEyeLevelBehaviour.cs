f5e0d504868f264c128178cee8bc58ce081291e6
Ilya Builuk <ilya.builuk@gmail.com>
2016-08-21 19:02:21 +0200

    demo: change bird-eye scene to use map data import
    Persistent storage requires strings to be committed which
    I don't want to do at the moment.

--- unity/demo/Assets/Scripts/BirdEyeLevelBehaviour.cs
+++ unity/demo/Assets/Scripts/BirdEyeLevelBehaviour.cs
@@ -4,6 +4,7 @@ using UtyMap.Unity.Core.Models;
 using UtyMap.Unity.Core.Tiling;
 using UtyMap.Unity.Core.Utils;
 using UtyMap.Unity.Infrastructure.Config;
+using UtyMap.Unity.Infrastructure.Primitives;
 using UtyMap.Unity.Maps.Data;
 using UtyRx;
 
@@ -14,10 +15,10 @@ namespace Assets.Scripts
         private const int LevelOfDetails = 14;
 
         /// <summary> Start latitude. </summary>
-        public double StartLatitude = 52.5169051;
+        public double StartLatitude = 52.53149;
 
         /// <summary> Start longitude. </summary>
-        public double StartLongitude = 13.3882853;
+        public double StartLongitude = 13.38787;
 
         private ApplicationManager _appManager;
 
@@ -42,6 +43,16 @@ namespace Assets.Scripts
             // utymap is better to start on non-UI thread
             Observable.Start(() =>
             {
+                // NOTE this is just example: you can load your regions into memory once
+                // game is started. Also you can specify different zoom level if you
+                // have valid mapcss stylesheet
+                _appManager
+                    .GetService<IMapDataLoader>()
+                    .AddToStore(MapStorageType.InMemory,
+                                @"Osm/berlin.osm.xml",
+                                _appManager.GetService<Stylesheet>(),
+                                new Range<int>(LevelOfDetails, LevelOfDetails));
+
                 _appManager.RunGame();
 
                 // NOTE Assume that persistent data is created.
@@ -59,25 +70,19 @@ namespace Assets.Scripts
             var mapDataLoader = _appManager.GetService<IMapDataLoader>();
 
             var centerQuadKey = GeoUtils.CreateQuadKey(new GeoCoordinate(StartLatitude, StartLongitude), LevelOfDetails);
-            int halfCount = 1;
-            for (var y = -halfCount; y < halfCount; ++y)
-                for (var x = -halfCount; x < halfCount; ++x)
+            var tile = new Tile(centerQuadKey, tileController.Stylesheet, tileController.Projection);
+            mapDataLoader
+                .Load(tile)
+                .SubscribeOn(Scheduler.CurrentThread)
+                .ObserveOn(Scheduler.MainThread)
+                .Subscribe(u =>
                 {
-                    var quadKey = new QuadKey(centerQuadKey.TileX + x, centerQuadKey.TileY + y, LevelOfDetails);
-                    var tile = new Tile(quadKey, tileController.Stylesheet, tileController.Projection);
-                    mapDataLoader
-                        .Load(tile)
-                        .SubscribeOn(Scheduler.CurrentThread)
-                        .ObserveOn(Scheduler.MainThread)
-                        .Subscribe(u =>
-                        {
-                            // NOTE can create game object only on UI thread
-                            if (tile.GameObject == null)
-                                tile.GameObject = new GameObject("tile");
+                    // NOTE can create game object only on UI thread
+                    if (tile.GameObject == null)
+                        tile.GameObject = new GameObject("tile");
 
-                            u.Match(e => modelBuilder.BuildElement(tile, e), m => modelBuilder.BuildMesh(tile, m));
-                        });
-                }
+                    u.Match(e => modelBuilder.BuildElement(tile, e), m => modelBuilder.BuildMesh(tile, m));
+                });
         }
     }
 }
