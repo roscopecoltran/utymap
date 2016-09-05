11e6ad3f683c51f6e5a2e673df54797e94bec25a
Ilya Builuk <ilya.builuk@gmail.com>
2016-09-05 22:26:44 +0200

    demo: add character protector script
    This should prevent character to fall when tile is not fully loaded.
    This is workaround.

---
+++ unity/demo/Assets/Scripts/CharacterProtector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary> This class is workaround for slow tile loading. </summary>
+    /// <remarks> 
+    ///     Actual reason can be slow internet connection, remote server
+    ///     performance, amount of map data to be processed. 
+    /// </remarks>
+    class CharacterProtector : MonoBehaviour
+    {
+        private GameObject _plane;
+
+        void Start()
+        {
+            _plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            _plane.name = "ProtectionPlane";
+            _plane.GetComponent<MeshRenderer>().enabled = false;
+            _plane.transform.localScale = new Vector3(0.5f, 0.1f, 0.5f);
+        }
+
+        void Update()
+        {
+            var position = gameObject.transform.position;
+            _plane.transform.position = new Vector3(position.x, -1.2f, position.z);
+        }
+    }
+}
