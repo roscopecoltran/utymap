0c3f2e733f8ced4c1086435c765a058dc919e512
Ilya Builuk <ilya.builuk@gmail.com>
2016-09-28 22:05:11 +0200

    core: add test stub for TextureAtlasParser

---
+++ core/test/mapcss/TextureAtlasParserTest.cpp
@@ -0,0 +1,14 @@
+#include "mapcss/TextureAtlasParser.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace utymap::mapcss;
+
+BOOST_AUTO_TEST_SUITE(MapCss_TextureAtlasParser)
+
+BOOST_AUTO_TEST_CASE(GivenSimpleContent_WhenParse_ThenAtlasReturned)
+{
+    // TODO
+}
+
+BOOST_AUTO_TEST_SUITE_END()
