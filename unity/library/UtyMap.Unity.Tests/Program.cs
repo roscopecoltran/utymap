ceaba456bd2048179c20f3e6fd61bc8b49e95cd0
Ilya Builuk <ilya.builuk@gmail.com>
2016-10-07 22:54:08 +0200

    unity: remove positioning API
    This api is not used so far. No need to keep it.

--- unity/library/UtyMap.Unity.Tests/Program.cs
+++ unity/library/UtyMap.Unity.Tests/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Threading;
-using UtyMap.Unity.Core;
-using UtyMap.Unity.Core.Positioning;
-using UtyMap.Unity.Core.Positioning.Nmea;
+﻿using UtyMap.Unity.Core;
 using UtyMap.Unity.Core.Tiling;
-using UtyMap.Unity.Infrastructure;
-using UtyMap.Unity.Infrastructure.Diagnostic;
 using UtyMap.Unity.Infrastructure.Primitives;
 using UtyMap.Unity.Maps.Data;
 using UtyMap.Unity.Tests.Helpers;
@@ -18,66 +11,27 @@ namespace UtyMap.Unity.Tests
     internal class Program
     {
         private const int LevelOfDetails = 16;
-        private const string TraceCategory = "test.app";
-
-        private readonly GeoCoordinate _worldZeroPoint = TestHelper.WorldZeroPoint;
-        private readonly ManualResetEvent _waitEvent = new ManualResetEvent(false);
-
-        private CompositionRoot _compositionRoot;
-        private IMessageBus _messageBus;
-        private ITileController _tileController;
-
-        private ITrace _trace;
 
         private static void Main(string[] args)
         {
-            var program = new Program();
-            program.Initialize();
-            program.RunMocker();
-        }
+            var compositionRoot = TestHelper.GetCompositionRoot(TestHelper.WorldZeroPoint);
 
-        private void Initialize()
-        {
-            // initialize services
-            _compositionRoot = TestHelper.GetCompositionRoot(_worldZeroPoint);
+            var tileController = compositionRoot.GetService<ITileController>();
+            var mapDataLoader = compositionRoot.GetService<IMapDataLoader>();
 
-            // get local references
-            _messageBus = _compositionRoot.GetService<IMessageBus>();
-            _trace = _compositionRoot.GetService<ITrace>();
-            _tileController = _compositionRoot.GetService<ITileController>();
-
-            SetupMapData();
-
-            // set observer to react on geo position changes
-            _messageBus.AsObservable<GeoPosition>()
-                .ObserveOn(Scheduler.MainThread)
-                .Subscribe(position =>
-            {
-                _trace.Debug(TraceCategory, "GeoPosition: {0}", position.ToString());
-                _tileController.OnPosition(position.Coordinate, LevelOfDetails);
-            });
-        }
-
-        /// <summary> Reads nmea file and sends fake geo positions via message bus. </summary>
-        private void RunMocker()
-        {
-            Action<TimeSpan> delayAction = Thread.Sleep;
-            using (Stream stream = new FileStream(TestHelper.NmeaFilePath, FileMode.Open))
-            {
-                var mocker = new NmeaPositionMocker(stream, _messageBus);
-                mocker.OnDone += (s, e) => _waitEvent.Set();
-                mocker.Start(delayAction);
-            }
-        }
-
-        /// <summary> Setup test map data. </summary>
-        private void SetupMapData()
-        {
-            var mapDataLoader = _compositionRoot.GetService<IMapDataLoader>();
-            var stylesheet = _compositionRoot.GetService<Stylesheet>();
-
-            mapDataLoader.AddToStore(MapStorageType.InMemory, TestHelper.BerlinXmlData, stylesheet,
+            mapDataLoader.AddToStore(MapStorageType.InMemory,
+                TestHelper.BerlinXmlData,
+                compositionRoot.GetService<Stylesheet>(),
                 new Range<int>(LevelOfDetails, LevelOfDetails));
+
+            tileController
+                .Where(tile => !tile.IsDisposed)
+                .SelectMany(tile => mapDataLoader.Load(tile))
+                .SubscribeOn(Scheduler.ThreadPool)
+                .ObserveOn(Scheduler.MainThread)
+                .Subscribe(u => { });
+
+            tileController.OnPosition(TestHelper.WorldZeroPoint, LevelOfDetails);
         }
     }
 }
