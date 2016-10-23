0ecd6264e3d8245ea86f8ae8076736e85053d6fb
Ilya Builuk <ilya.builuk@gmail.com>
2016-10-23 18:44:54 +0200

    unity: add mapzen elevation provider

---
+++ unity/library/UtyMap.Unity.Tests/Maps/Providers/MapzenElevationDataProviderTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using UtyDepend.Config;
+using UtyMap.Unity.Core;
+using UtyMap.Unity.Core.Tiling;
+using UtyMap.Unity.Core.Utils;
+using UtyMap.Unity.Infrastructure.Diagnostic;
+using UtyMap.Unity.Infrastructure.IO;
+using UtyMap.Unity.Maps.Providers;
+using UtyMap.Unity.Tests.Helpers;
+using UtyRx;
+
+namespace UtyMap.Unity.Tests.Maps.Providers
+{
+    [TestFixture]
+    public class MapzenElevationDataProviderTests
+    {
+        private Mock<IFileSystemService> _fileSystemService;
+        private Mock<INetworkService> _networkService;
+        private Mock<ITrace> _trace;
+        private Mock<IConfigSection> _config;
+        private Mock<Stream> _writeStream;
+        
+        private Tile _tile;
+        private byte[] _responseBytes = new byte[8];
+
+        private MapzenElevationDataProvider _eleProvider;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _fileSystemService = new Mock<IFileSystemService>();
+            _networkService = new Mock<INetworkService>();
+            _trace = new Mock<ITrace>();
+
+            _config = new Mock<IConfigSection>();
+            _config.Setup(c => c.GetString("data/mapzen/ele_server", It.IsAny<string>())).Returns("elevation.mapzen.com/height?json={0}&api_key={1}");
+            _config.Setup(c => c.GetString("data/mapzen/api_key", It.IsAny<string>())).Returns("ggg");
+            _config.Setup(c => c.GetString("data/mapzen/ele_format", It.IsAny<string>())).Returns("ele");
+            _config.Setup(c => c.GetInt("data/mapzen/ele_grid", It.IsAny<int>())).Returns(2);
+            _config.Setup(c => c.GetString("data/cache", It.IsAny<string>())).Returns("Cache");
+
+            _writeStream = new Mock<Stream>();
+
+            _tile = new Tile(GeoUtils.CreateQuadKey(TestHelper.WorldZeroPoint, 16), new Stylesheet(""), 
+                new CartesianProjection(TestHelper.WorldZeroPoint));
+
+            _networkService
+                .Setup(ns => ns.GetAndGetBytes(It.IsAny<string>(), It.IsAny<Dictionary<string,string>>()))
+                .Returns(Observable.Return(_responseBytes));
+
+            _fileSystemService
+                .Setup(fs => fs.WriteStream(It.IsAny<string>()))
+                .Returns(_writeStream.Object);
+            
+            _eleProvider = new MapzenElevationDataProvider(_fileSystemService.Object, _networkService.Object, _trace.Object);
+
+            _eleProvider.Configure(_config.Object);
+        }
+
+        [Test]
+        public void CanRequestPolyline()
+        {
+            _eleProvider.Get(_tile).Wait();
+
+            _networkService.Verify(ns => ns.GetAndGetBytes("elevation.mapzen.com/height?json={\"range\":false,\"encoded_polyline\":\"kzcecBqdapX?sjDmgBrjD?sjD\"}&api_key=ggg", It.IsAny<Dictionary<string, string>>()));
+        }
+
+        [Test]
+        public void CanCreateDataFile()
+        {
+            _eleProvider.Get(_tile).Wait();
+
+            _fileSystemService.Verify(fs => fs.WriteStream("Cache\\1202102332220103.ele"));
+        }
+
+        [Test]
+        public void CanWriteCorrectEleData()
+        {
+            _eleProvider.Get(_tile).Wait();
+
+            _writeStream.Verify(ws => ws.Write(_responseBytes, 0, _responseBytes.Length));
+        }
+    }
+}
---
+++ unity/library/UtyMap.Unity/Maps/Providers/MapzenElevationDataProvider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UtyDepend;
+using UtyDepend.Config;
+using UtyMap.Unity.Core;
+using UtyMap.Unity.Core.Tiling;
+using UtyMap.Unity.Core.Utils;
+using UtyMap.Unity.Infrastructure.Diagnostic;
+using UtyMap.Unity.Infrastructure.IO;
+using UtyRx;
+
+namespace UtyMap.Unity.Maps.Providers
+{
+    public class MapzenElevationDataProvider : MapDataProvider, IConfigurable
+    {
+        private string _mapDataServerUri;
+        private string _mapDataApiKey;
+        private string _cachePath;
+        private int _eleGrid;
+        private string _mapDataFormatExtension;
+
+        [Dependency]
+        public MapzenElevationDataProvider(IFileSystemService fileSystemService, INetworkService networkService, ITrace trace) :
+            base(fileSystemService, networkService, trace)
+        {
+        }
+
+        /// <inheritdoc />
+        public override IObservable<string> Get(Tile tile)
+        {
+            var filePath = Path.Combine(_cachePath, tile.QuadKey + _mapDataFormatExtension);
+            var uri = String.Format(_mapDataServerUri, GetJsonPayload(tile.QuadKey), _mapDataApiKey);
+
+            return Get(tile, uri, filePath);
+        }
+
+        private string GetJsonPayload(QuadKey quadkey)
+        {
+            return String.Format("{{\"range\":false,\"encoded_polyline\":\"{0}\"}}",
+                EncodePolyline(CreatePolyline(quadkey)));
+        }
+
+        private IEnumerable<GeoCoordinate> CreatePolyline(QuadKey quadkey)
+        {
+            var bbox = GeoUtils.QuadKeyToBoundingBox(quadkey);
+
+            var latStep = (bbox.MaxPoint.Latitude - bbox.MinPoint.Latitude) / _eleGrid;
+            var lonStep = (bbox.MaxPoint.Longitude - bbox.MinPoint.Longitude) / _eleGrid;
+
+            for (int i = 0; i < _eleGrid; ++i)
+            {
+                var lat = bbox.MinPoint.Latitude + i * latStep;
+                for (int j = 0; j < _eleGrid; ++j)
+                {
+                    var lon = bbox.MinPoint.Longitude + j * lonStep;
+                    yield return new GeoCoordinate(lat, lon);
+                }
+            }
+        }
+
+        private static string EncodePolyline(IEnumerable<GeoCoordinate> points)
+        {
+            const double precision = 1E6;
+
+            var str = new StringBuilder();
+            Action<int> encodeDiff = (diff =>
+            {
+                int shifted = diff << 1;
+                if (diff < 0)
+                    shifted = ~shifted;
+                int rem = shifted;
+                while (rem >= 0x20)
+                {
+                    str.Append((char) ((0x20 | (rem & 0x1f)) + 63));
+                    rem >>= 5;
+                }
+                str.Append((char) (rem + 63));
+            });
+
+            int lastLat = 0;
+            int lastLng = 0;
+            foreach (var point in points)
+            {
+                int lat = (int) Math.Round(point.Latitude*precision);
+                int lng = (int) Math.Round(point.Longitude*precision);
+                encodeDiff(lat - lastLat);
+                encodeDiff(lng - lastLng);
+                lastLat = lat;
+                lastLng = lng;
+            }
+            return str.ToString();
+        }
+
+        /// <inheritdoc />
+        public void Configure(IConfigSection configSection)
+        {
+            _mapDataServerUri = configSection.GetString(@"data/mapzen/ele_server", null);
+            _mapDataApiKey = configSection.GetString(@"data/mapzen/api_key", null);
+            _mapDataFormatExtension = "." + configSection.GetString(@"data/mapzen/ele_format", "ele");
+            _eleGrid = configSection.GetInt(@"data/mapzen/ele_grid", 10);
+
+            _cachePath = configSection.GetString(@"data/cache", null);
+        }
+    }
+}
