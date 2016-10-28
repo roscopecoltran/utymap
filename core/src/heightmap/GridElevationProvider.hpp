83b48b2b8c17c556d1f5be3efe19dec70699e997
Ilya Builuk <ilya.builuk@gmail.com>
2016-10-28 20:44:40 +0200

    core: add grid elevation provider implementation

--- core/src/heightmap/GridElevationProvider.hpp
+++ core/src/heightmap/GridElevationProvider.hpp
@@ -2,21 +2,41 @@
 #define HEIGHTMAP_GRIDELEVATIONPROVIDER_HPP_DEFINED
 
 #include "heightmap/ElevationProvider.hpp"
+#include "utils/CoreUtils.hpp"
 #include "utils/GeoUtils.hpp"
 
+#include <algorithm>
+#include <cmath>
+#include <fstream>
 #include <string>
 #include <stdexcept>
 #include <sstream>
+#include <map>
+#include <vector>
 
 namespace utymap { namespace heightmap {
 
-// Provides the way to get elevation for given location from grid 
-// represented by comma separated list of integers
+/// Provides the way to get elevation for given location from grid 
+/// represented by comma separated list of integers
 class GridElevationProvider final : public ElevationProvider
 {
+    /// Holds elevation data information.
+    struct EleData
+    {
+        int xStart = 0;
+        int yStart = 0;
+        int xStep = 0;
+        int yStep = 0;
+        int resolution = 0;
+
+        std::vector<int> heights;
+    };
+
+    const double Scale = 1E7;
+
 public:
     GridElevationProvider(std::string dataDirectory) :
-        dataDirectory_(dataDirectory)
+        dataDirectory_(dataDirectory), data_()
     {
     }
 
@@ -26,21 +46,83 @@ public:
     }
 
     /// Preloads data for given quadkey.
-    void preload(const utymap::QuadKey& quadkey) override 
+    void preload(const utymap::QuadKey& quadKey) override
     {
+        if (data_.find(quadKey) != data_.end())
+            return;
 
+        std::fstream file(getFilePath(quadKey));
+        if (!file.good())
+            throw std::invalid_argument("Cannot find elevation file.");
+        
+        EleData data;
+
+        std::transform(std::istream_iterator<std::string>(file),
+                       std::istream_iterator<std::string>(),
+                       std::back_inserter(data.heights),
+                       [&](const std::string& heightString) {
+                           return utymap::utils::lexicalCast<int>(heightString);
+        });
+
+        data.heights.shrink_to_fit();
+
+        if (data.heights.empty())
+            throw std::domain_error("Cannot get elevation data.");
+
+        BoundingBox bbox = utymap::utils::GeoUtils::quadKeyToBoundingBox(quadKey);
+
+        data.resolution = static_cast<int>(std::sqrt(data.heights.size())) - 1;
+        data.xStart = static_cast<int>(bbox.minPoint.longitude * Scale);
+        data.yStart = static_cast<int>(bbox.minPoint.latitude * Scale);
+        data.xStep = static_cast<int>(bbox.width() / data.resolution * Scale);
+        data.yStep = static_cast<int>(bbox.height() / data.resolution * Scale);
+
+        data_.insert(std::make_pair(quadKey, std::move(data)));
     }
 
     /// Gets elevation for given geocoordinate.
     double getElevation(const utymap::QuadKey& quadKey, const utymap::GeoCoordinate& coordinate) const override
     {
-        return 0;
+        return getElevation(quadKey, coordinate.latitude, coordinate.longitude);
     }
 
     /// Gets elevation for given geocoordinate.
     double getElevation(const utymap::QuadKey& quadKey, double latitude, double longitude) const override
     {
-        return 0;
+        auto data = data_.find(quadKey);
+        if (data == data_.end())
+            throw std::domain_error("Elevation for given quadkey is not preloaded!");
+
+        int resolution = data->second.resolution;
+
+        int x = static_cast<int>(longitude * Scale) - data->second.xStart;
+        int y = static_cast<int>(latitude * Scale) - data->second.yStart;
+
+        int x0 = x / data->second.xStep;
+        int y0 = y / data->second.yStep;
+
+        int x1 = std::min(x0 + 1, resolution);
+        int y1 = std::min(y0 + 1, resolution);
+
+        double dx = static_cast<double>(x - x0 * data->second.xStep) / data->second.xStep;
+        double dy = static_cast<double>(y - y0 * data->second.yStep) / data->second.yStep;
+
+        int cellSize = resolution + 1;
+        int height2 = data->second.heights[x0 + y0 * cellSize];
+        int height0 = data->second.heights[x0 + y1 * cellSize];
+        int height3 = data->second.heights[x1 + y0 * cellSize];
+        int height1 = data->second.heights[x1 + y1 * cellSize];
+
+        // Bilinear interpolation
+        // h0------------h1
+        // |
+        // |--dx-- .
+        // |       |
+        // |      dy
+        // |       |
+        // h2------------h3
+
+        return height0*dy*(1 - dx) + height1*dy*(dx)+height2*(1 - dy)*(1 - dx) + height3*(1 - dy)*dx;
     }
 
 private:
@@ -53,6 +135,7 @@ private:
     }
 
     const std::string dataDirectory_;
+    std::map<const QuadKey, EleData, QuadKey::Comparator> data_;
 };
 
 }}
--- core/test/heightmap/GridElevationProviderTest.cpp
+++ core/test/heightmap/GridElevationProviderTest.cpp
@@ -6,11 +6,71 @@
 using namespace utymap;
 using namespace utymap::heightmap;
 
-BOOST_AUTO_TEST_SUITE(Heightmap_GridElevationProvider)
+namespace {
 
-BOOST_AUTO_TEST_CASE(GivenTestLocation_WhenGetElevation_ThenReturnExpectedInteger)
+    // Elevation data for given quadkey is:
+    //  3  4  5
+    //  0  1  2
+    // -3 -2 -1
+    struct Heightmap_GridElevationProviderFixture
+    {
+        Heightmap_GridElevationProviderFixture() :
+            quadKey(16, 35205, 21489),
+            bbox(utymap::utils::GeoUtils::quadKeyToBoundingBox(quadKey)),
+            eleProvider(TEST_ELEVATION_DIRECTORY)
+        {
+            eleProvider.preload(quadKey);
+        }
+
+        QuadKey quadKey;
+        BoundingBox bbox;
+        GridElevationProvider eleProvider;
+    };
+
+    const double Precision = 0.1;
+}
+
+BOOST_FIXTURE_TEST_SUITE(Heightmap_GridElevationProvider, Heightmap_GridElevationProviderFixture)
+
+BOOST_AUTO_TEST_CASE(GivenLocationAtBottomLeft_WhenGetElevation_ThenReturnExpectedNegativeHeight)
 {
-    // TODO
+    double ele = eleProvider.getElevation(quadKey, bbox.minPoint);
+
+    BOOST_CHECK_CLOSE(ele, -3, Precision);
+}
+
+BOOST_AUTO_TEST_CASE(GivenLocationAtTopRight_WhenGetElevation_ThenReturnExpectedPositiveHeight)
+{
+    double ele = eleProvider.getElevation(quadKey, bbox.maxPoint);
+
+    BOOST_CHECK_CLOSE(ele, 5, Precision);
+}
+
+BOOST_AUTO_TEST_CASE(GivenLocationInCenter_WhenGetElevation_ThenReturnExpectedHeight)
+{
+    double ele = eleProvider.getElevation(quadKey, bbox.center());
+
+    BOOST_CHECK_CLOSE(ele, 1, Precision);
+}
+
+BOOST_AUTO_TEST_CASE(GivenLocationInTheMiddleFromCenterToTopRight_WhenGetElevation_ThenReturnExpectedInterpolatedHeight)
+{
+    GeoCoordinate coordinate(bbox.center().latitude + bbox.height() / 4, 
+                             bbox.center().longitude + bbox.width() / 4);
+
+    double ele = eleProvider.getElevation(quadKey, coordinate);
+
+    BOOST_CHECK_CLOSE(ele, 3, Precision);
+}
+
+BOOST_AUTO_TEST_CASE(GivenLocationNearTopRight_WhenGetElevation_ThenReturnExpectedInterpolatedHeight)
+{
+    GeoCoordinate coordinate(bbox.maxPoint.latitude, 
+                             bbox.center().longitude + bbox.width() / 4);
+
+    double ele = eleProvider.getElevation(quadKey, coordinate);
+
+    BOOST_CHECK_CLOSE(ele, 4.5, Precision);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
