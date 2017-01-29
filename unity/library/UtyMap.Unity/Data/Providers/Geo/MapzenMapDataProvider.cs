4d23b6b466448b70542e0ed47d8b03d04c4bde94
Ilya Builuk <ilya.builuk@gmail.com>
2017-01-29 10:39:51 +0100

    demo: add zoom level mapping

--- unity/library/UtyMap.Unity/Data/Providers/Geo/MapzenMapDataProvider.cs
+++ unity/library/UtyMap.Unity/Data/Providers/Geo/MapzenMapDataProvider.cs
@@ -14,7 +14,7 @@ namespace UtyMap.Unity.Data.Providers.Geo
         /// <summary> Provides the way to control what to request from server. </summary>
         private static readonly Dictionary<int, int> LodMapping = new Dictionary<int, int>()
         {
-            {2, 1}
+            {2, 1}, {3, 1}
         };
 
         private string _cachePath;
