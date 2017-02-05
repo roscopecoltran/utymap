93904e6f213911e25816ab46921b92b8ef43cba7
Ilya Builuk <ilya.builuk@gmail.com>
2017-02-05 21:16:43 +0100

    demo: commit progress on scenes

--- unity/demo/Assets/Scripts/Scene/Builders/LabelElementBuilder.cs
+++ unity/demo/Assets/Scripts/Scene/Builders/LabelElementBuilder.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Scenes.Orbit.Scripts;
+using Assets.Scenes.Surface.Scripts;
 using Assets.Scripts.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UtyMap.Unity;
 using UtyMap.Unity.Utils;
 
@@ -23,8 +26,19 @@ namespace Assets.Scripts.Scene.Builders
         public GameObject Build(Tile tile, Element element)
         {
             var gameObject = new GameObject(GetName(element));
+            // NOTE should be attached to properly oriented canvas from proper scene.
+            gameObject.transform.SetParent(_canvas.transform);
+
+            return element.Styles["type"] == "flat"
+                ? BuildFlatText(gameObject, element)
+                : BuildSphereText(gameObject, element);
+        }
+
+        /// <summary> Builds text on sphere. </summary>
+        private GameObject BuildSphereText(GameObject gameObject, Element element)
+        {
             var sphereText = gameObject.AddComponent<SphereText>();
-            
+
             sphereText.Coordinate = element.Geometry[0];
             // NOTE should be in sync with sphere size and offsetted polygons
             sphereText.Radius = OrbitCalculator.Radius + 75;
@@ -36,8 +50,28 @@ namespace Assets.Scripts.Scene.Builders
             sphereText.text = GetText(element);
             sphereText.alignment = TextAnchor.MiddleCenter;
            
-            // NOTE should be attached to properly oriented canvas.
-            gameObject.transform.SetParent(_canvas.transform);
+            return gameObject;
+        }
+
+        /// <summary> Builds flat text. </summary>
+        private GameObject BuildFlatText(GameObject gameObject, Element element)
+        {
+            var text = gameObject.AddComponent<TextMesh>();
+
+            var position2D = GeoUtils.ToMapCoordinate(SurfaceCalculator.GeoOrigin, element.Geometry[0]);
+            // TODO determine correct height.
+            gameObject.transform.position = new Vector3(position2D.x, 1000, position2D.y);
+            gameObject.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+            var font = new FontWrapper(element.Styles);
+            //text.font = font.Font;
+            text.fontSize = font.Size;
+            text.color = font.Color;
+            text.text = GetText(element);
+            text.anchor = TextAnchor.MiddleCenter;
+            text.alignment = TextAlignment.Center;
+
+            gameObject.transform.localScale *= font.Scale;
 
             return gameObject;
         }
@@ -58,12 +92,16 @@ namespace Assets.Scripts.Scene.Builders
             public readonly Font Font;
             public readonly int Size;
             public readonly Color Color;
+            public readonly int Scale;
 
             public FontWrapper(Dictionary<string, string> styles)
             {
                 Size = int.Parse(styles["font-size"]);
                 Font = UnityEngine.Font.CreateDynamicFontFromOSFont(styles["font-name"], Size);
                 Color = ColorUtils.FromUnknown(styles["font-color"]);
+
+                if (!styles.ContainsKey("font-scale") || !int.TryParse(styles["font-scale"], out Scale)) 
+                    Scale = 1;
             }
         }
     }
